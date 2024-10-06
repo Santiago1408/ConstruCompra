5db5cc069a6264f67cb6d2e102778f22fefe2253
mlfc0106 <fernandez.campos.maria.laura@gmail.com>
2024-10-05 21:56:25 -0400

    Update marketplace.sql

--- marketplace.sql
+++ marketplace.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump 
 -- version 6.0.0-dev
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 192.168.30.22
--- Tiempo de generación: 05-10-2024 a las 00:48:04
+-- Tiempo de generación: 05-10-2024 a las 00:24:59
 -- Versión del servidor: 10.4.8-MariaDB-1:10.4.8+maria~stretch-log
 -- Versión de PHP: 8.2.20
 
@@ -20,16 +20,12 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `marketplace`
 --
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
   `id_producto` int(11) NOT NULL,
-  `id_usuarios` int(11) NOT NULL,
+  `id_vendedor` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL CHECK (char_length(`nombre`) >= 3 and char_length(`nombre`) <= 25),
   `descripcion` varchar(100) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL CHECK (`cantidad` > 0),
@@ -42,85 +38,48 @@ CREATE TABLE `productos` (
   `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`id_producto`, `id_usuarios`, `nombre`, `descripcion`, `cantidad`, `tipo_venta`, `precio`, `ubicacion`, `marca`, `color`, `tamano`, `fecha_publicacion`) VALUES
-(1, 1, 'Cemento', 'Bolsa de cemento Portland de 50kg', 100, 'Total', 45.50, 'Calle 123, Cochabamba', 'Fancesa', 'Gris', '50kg', '2024-10-05 00:47:51'),
-(2, 2, 'Ladrillos', 'Ladrillos tipo King Kong 18x18x33 cm', 500, 'Unitario', 1.20, 'Av. Libertad 456, Cochabamba', 'Ladrix', 'Rojo', '18x18x33 cm', '2024-10-05 00:47:51'),
-(3, 3, 'Arena', 'Metro cúbico de arena fina para construcción', 10, 'Total', 35.00, 'Calle de los Álamos 789, Cochabamba', NULL, 'Amarillo', '1 m3', '2024-10-05 00:47:51'),
-(4, 4, 'Pintura acrílica', 'Galón de pintura acrílica blanca', 50, 'Unitario', 75.00, 'Av. América 321, Cochabamba', 'Sipa', 'Blanco', '1 galón', '2024-10-05 00:47:51'),
-(5, 5, 'Varilla de acero', 'Varilla de acero corrugado de 12mm', 200, 'Unitario', 25.00, 'Av. Melchor Pérez 654, Cochabamba', 'Acerbol', 'Plateado', '12mm', '2024-10-05 00:47:51'),
-(6, 6, 'Azulejos', 'Paquete de 1m2 de azulejos cerámicos', 50, 'Total', 120.00, 'Calle Tiquipaya 135, Cochabamba', 'Keramia', 'Blanco', '1m2', '2024-10-05 00:47:51'),
-(7, 7, 'Grava', 'Metro cúbico de grava gruesa', 15, 'Total', 40.00, 'Av. Blanco Galindo 246, Cochabamba', NULL, 'Gris', '1 m3', '2024-10-05 00:47:51'),
-(8, 8, 'Puerta de madera', 'Puerta de madera maciza de 2x0.8m', 20, 'Unitario', 250.00, 'Calle Aranjuez 357, Cochabamba', 'Maderco', 'Marrón', '2x0.8m', '2024-10-05 00:47:51'),
-(9, 9, 'Tejas', 'Tejas cerámicas para techos', 300, 'Unitario', 5.00, 'Av. Beijing 468, Cochabamba', 'CeramTec', 'Rojo', '30x30cm', '2024-10-05 00:47:51'),
-(10, 10, 'Tubería PVC', 'Tubería PVC de 4 pulgadas', 100, 'Total', 30.00, 'Calle Cobija 579, Cochabamba', 'Plastibol', 'Blanco', '4 pulgadas', '2024-10-05 00:47:51'),
-(11, 11, 'Malla electrosoldada', 'Malla electrosoldada de 6x6mm', 50, 'Unitario', 100.00, 'Av. Circunvalación 680, Cochabamba', 'Metaldom', 'Plateado', '6x6mm', '2024-10-05 00:47:51'),
-(12, 12, 'Ventana de aluminio', 'Ventana de aluminio de 1.2x1m', 25, 'Unitario', 150.00, 'Av. Villazón 791, Cochabamba', 'Alumco', 'Gris', '1.2x1m', '2024-10-05 00:47:51'),
-(13, 13, 'Yeso', 'Bolsa de yeso de 25kg', 80, 'Total', 30.00, 'Calle Lanza 902, Cochabamba', 'YesBol', 'Blanco', '25kg', '2024-10-05 00:47:51'),
-(14, 14, 'Cal', 'Bolsa de cal de 30kg', 60, 'Total', 35.00, 'Av. Salamanca 135, Cochabamba', 'Fancesa', 'Blanco', '30kg', '2024-10-05 00:47:51'),
-(15, 15, 'Grifería', 'Juego de grifería cromada para baño', 40, 'Unitario', 200.00, 'Calle Tomás Frías 246, Cochabamba', 'Grifor', 'Plateado', 'Estándar', '2024-10-05 00:47:51'),
-(16, 16, 'Lámina de yeso', 'Lámina de yeso de 2.4x1.2m', 100, 'Unitario', 80.00, 'Calle Mairana 357, Cochabamba', 'YesCoch', 'Blanco', '2.4x1.2m', '2024-10-05 00:47:51'),
-(17, 17, 'Cemento cola', 'Bolsa de cemento cola para cerámica', 150, 'Total', 45.00, 'Av. Petrolera 468, Cochabamba', 'AdhesBol', 'Gris', '25kg', '2024-10-05 00:47:51'),
-(18, 18, 'Luminaria LED', 'Lámpara LED de techo 18W', 75, 'Unitario', 60.00, 'Calle Jordan 579, Cochabamba', 'LumiTec', 'Blanco', '18W', '2024-10-05 00:47:51'),
-(19, 19, 'Piso flotante', 'Paquete de 2m2 de piso flotante', 25, 'Total', 300.00, 'Calle España 680, Cochabamba', 'PisoFlex', 'Madera', '2m2', '2024-10-05 00:47:51'),
-(20, 20, 'Interruptor eléctrico', 'Interruptor eléctrico de pared', 100, 'Unitario', 20.00, 'Av. América Oeste 791, Cochabamba', 'Elec', 'Blanco', '15 cm', '2024-10-05 00:47:51');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `Usuario`
 --
 
-CREATE TABLE `usuarios` (
-  `id_usuarios` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
-  `direccion` varchar(100) DEFAULT NULL,
-  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
+CREATE TABLE `Usuario` (
+  `NombreUsr` varchar(100) NOT NULL,
+  `CorreoElectronicoUsr` varchar(100) NOT NULL,
+  `FechaNacimientoUsr` date NOT NULL,
+  `GeneroUsr` varchar(20) DEFAULT NULL,
+  `DireccionUsr` varchar(300) NOT NULL,
+  `TelfUsr` varchar(15) DEFAULT NULL,
+  `ContraseniaUsr` varchar(100) NOT NULL,
+  `codUsr` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `productos`
+-- Índices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_usuarios` (`id_usuarios`);
+  ADD KEY `id_vendedor` (`id_vendedor`);
 
 --
--- Indices de la tabla `usuarios`
+-- Índices de la tabla `Usuario`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuarios`),
-  ADD UNIQUE KEY `correo` (`correo`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
+ALTER TABLE `Usuario`
+  ADD PRIMARY KEY (`codUsr`);
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `Usuario`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+ALTER TABLE `Usuario`
+  MODIFY `codUsr` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -130,7 +89,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id_usuarios`) ON DELETE CASCADE;
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_vendedor`) REFERENCES `Usuario` (`codUsr`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
