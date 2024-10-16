fc0a45d3601fd3b8ab40583e00e55b8b2e7d9d14
mlfc0106 <fernandez.campos.maria.laura@gmail.com>
2024-10-16 09:05:05 -0400

    Insercción de campos en tabla Productos

--- marketplace.sql
+++ marketplace.sql
@@ -3,14 +3,19 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+
 CREATE TABLE `productos` (
   `id_producto` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL, 
-  `descripcion` varchar(500) DEFAULT NULL, 
-  `cantidad` int(11) DEFAULT NULL, 
-  `precio` decimal(10,2) NOT NULL, 
-  `ubicacion` varchar(500) NOT NULL, 
-  `tamano` varchar(50) DEFAULT NULL, 
+  `id_usuarios` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `tipo_venta` enum('Unitario','Total') NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `color` varchar(50) DEFAULT NULL,
+  `tamano` varchar(50) DEFAULT NULL,
   `fecha_publicacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
