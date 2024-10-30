2c2ff264a71d2598e1533906f15c8a2248561580
mlfc0106 <fernandez.campos.maria.laura@gmail.com>
2024-10-29 21:01:13 -0400

    Agregar función para ver perfil usuario

--- app.py
+++ app.py
@@ -205,3 +205,22 @@ def mis_publicaciones():
     except Exception as e:
         print(f"Error: {e}")  # Ver error en consola
         return "Ocurrió un error en el servidor", 500
+
+@app.route('/ver_perfil/<int:user_id>')
+def ver_perfil_usuario(user_id):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    query = '''
+        SELECT nombre, fecha_nacimiento, genero, direccion, telefono, correo
+        FROM usuarios
+        WHERE id_usuarios = %s
+    '''
+    cursor.execute(query, (user_id,))
+    datos_usuario = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    
+    # Verificar si el usuario que está viendo el perfil es el dueño
+    is_owner = 'id_usuario' in session and session['id_usuario'] == user_id
+    
+    return render_template('ver_perfil.html', datos_usuario=datos_usuario, is_owner=is_owner)
